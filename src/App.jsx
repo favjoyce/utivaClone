605243b842c2874dfb10d566889719ab413a4028
favjoyce <anyiafavour15@gmail.com>
2025-07-26 09:50:29 +0100

    Fix case-sensitive Header import

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './component/header';
+import Header from './component/Header';
 import Button from './component/Button'
-import Hero from './component/hero'
+import Hero from './component/Hero'
 
 function App() {
   
